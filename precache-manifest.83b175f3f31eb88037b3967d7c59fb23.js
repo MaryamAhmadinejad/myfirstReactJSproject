59966883edebeaec80d41c7588afb0092a92e9e8
maryam <maryam.ahmadinejad91@gmail.com>
2020-01-15 03:40:55 -0800

    Updates

---
+++ precache-manifest.83b175f3f31eb88037b3967d7c59fb23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ebb56d62eda91e699cfdc0dbfc067f",
+    "url": "/myfirstReactJSproject/index.html"
+  },
+  {
+    "revision": "6dda967f197f463d18f8",
+    "url": "/myfirstReactJSproject/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c5e9a8d39e18d553f88e",
+    "url": "/myfirstReactJSproject/static/js/2.253ae9e9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myfirstReactJSproject/static/js/2.253ae9e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6dda967f197f463d18f8",
+    "url": "/myfirstReactJSproject/static/js/main.e27f771c.chunk.js"
+  },
+  {
+    "revision": "05349479e252b812e714",
+    "url": "/myfirstReactJSproject/static/js/runtime-main.0ab3d290.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myfirstReactJSproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
